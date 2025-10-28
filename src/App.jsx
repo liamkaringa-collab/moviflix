1f24cdf251a40f97849254e06ec2f9702f31a4b6
liamkaringa-collab <juniorkaringa@gmail.com>
2025-10-28 14:59:49 +0300

    I have created a search in my header

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export const App = () => {
+  return (
+    <main>
+      <div className="pattern"/> 
+
+      <div className="wrapper">
+        <header>
+          <h1>Find <span className="text-gradient">Movies</span> You'll Enjoy Without the Hassle</h1>
+        </header>  
+
+        <p>Search</p> 
+      </div>
+    </main>
+  )
+}
+
+export default App 
