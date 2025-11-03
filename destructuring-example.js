1800bd258939a460ea2fdbe7730f18448e95d38d
liamkaringa-collab <juniorkaringa@gmail.com>
2025-11-03 11:17:21 +0300

    it is showing how to destructure in js

---
+++ destructuring-example.js
@@ -0,0 +1,12 @@
+// Object destructuring example
+const person = {
+    name: 'Bruce Wayne',
+    age: 36,
+    location: 'Gotham City'
+}
+
+// Destructuring assignment - extracting properties from the object
+const {name, age, location} = person;
+
+// Logging the destructured variable
+console.log(name) // Bruce Wayne
